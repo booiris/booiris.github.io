96c7bf15e1d0989b75588a93dc2bebe91de53681
booiris <booiris02@gmail.com>
2024-04-18 01:26:50 +0800

    Thu Apr 18 01:26:45 AM CST 2024

--- sync.sh
+++ sync.sh
@@ -4,12 +4,12 @@ cd $(dirname $0)
 rm -rf ./source/_posts/
 rm -rf ./source/wiki/
 # rm -rf ./source/game/
-rm -rf ./source/about/
+# rm -rf ./source/about/
 # rm -rf ./source/pic/
 \cp -a ../knowledge/note/data/notes/pages/ ./source/_posts/
 \cp -a ../knowledge/note/data/notes/wiki ./source
 # \cp -a ../knowledge/note/data/notes/game ./source
-\cp -a ../knowledge/note/data/notes/about ./source
+# \cp -a ../knowledge/note/data/notes/about ./source
 # \cp -a ../knowledge/note/data/notes/pic ./source
 git pull
 git add .
