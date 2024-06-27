505cb1388d3d16bb0547eb3c6ac644d6e304803a
booiris <booiris02@gmail.com>
2024-06-28 00:01:01 +0800

    Site updated: 2024-06-28 00:01:01

---
+++ game/push-box/pkg/push_box_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_game_free(a: number): void;
+export function game_tick(a: number): void;
+export function game_new(): number;
+export function game_width(a: number): number;
+export function game_height(a: number): number;
+export function game_room(a: number): number;
+export function game_game_over(a: number): number;
+export function game_restart(a: number): void;
+export function game_set_is_log(a: number, b: number): void;
+export function __wbindgen_free(a: number, b: number): void;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_exn_store(a: number): void;
