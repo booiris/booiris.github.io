047c17c9f2c3388109c0feb9100df99f81ff8147
booiris <booiris02@gmail.com>
2024-10-18 13:00:05 +0800

    Fri Oct 18 01:00:00 PM CST 2024

--- sync.sh
+++ sync.sh
@@ -11,6 +11,7 @@ rm -rf ./source/wiki/
 # \cp -a ../knowledge/note/data/notes/game ./source
 # \cp -a ../knowledge/note/data/notes/about ./source
 # \cp -a ../knowledge/note/data/notes/pic ./source
+
 git pull
 git add .
 git commit -m "$now_time"
