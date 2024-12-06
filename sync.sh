00f37a722de1a84fc094e13f7762ca1e701583ae
booiris <booiris02@gmail.com>
2024-12-06 23:52:36 +0800

    Fri Dec  6 11:52:32 PM CST 2024

--- sync.sh
+++ sync.sh
@@ -6,7 +6,14 @@ rm -rf ./source/wiki/
 # rm -rf ./source/game/
 # rm -rf ./source/about/
 # rm -rf ./source/pic/
-\cp -a ../knowledge/note/data/notes/pages/ ./source/_posts/
+# \cp -a ../knowledge/note/data/notes/pages/ ./source/_posts/
+find ../knowledge/note/data/notes/pages/ -type f -name "*.md" | while read file; do
+    if ! grep -q "published: false" "$file"; then
+        relative_path=${file#../knowledge/note/data/notes/pages/}
+        mkdir -p "./source/_posts/$(dirname "$relative_path")"
+        cp "$file" "./source/_posts/$relative_path"
+    fi
+done
 \cp -a ../knowledge/note/data/notes/wiki ./source
 # \cp -a ../knowledge/note/data/notes/game ./source
 # \cp -a ../knowledge/note/data/notes/about ./source
