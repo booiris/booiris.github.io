344e57066d0cf805d777b4ff6017203537fca4f7
booiris <booiris02@gmail.com>
2024-12-07 00:04:23 +0800

    Sat Dec  7 12:04:19 AM CST 2024

--- sync.sh
+++ sync.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 now_time=$(date)
 cd $(dirname $0)
-rm -rf ./source/_posts/
-rm -rf ./source/wiki/
+# rm -rf ./source/_posts/
+# rm -rf ./source/wiki/
 # rm -rf ./source/game/
 # rm -rf ./source/about/
 # rm -rf ./source/pic/
 # \cp -a ../knowledge/note/data/notes/pages/ ./source/_posts/
-find ../knowledge/note/data/notes/pages/ -type f -name "*.md" | while read file; do
-    if ! grep -q "published: false" "$file"; then
-        relative_path=${file#../knowledge/note/data/notes/pages/}
-        mkdir -p "./source/_posts/$(dirname "$relative_path")"
-        cp "$file" "./source/_posts/$relative_path"
-    fi
-done
-\cp -a ../knowledge/note/data/notes/wiki ./source
+# find ../knowledge/note/data/notes/pages/ -type f -name "*.md" | while read file; do
+#     if ! grep -q "published: false" "$file"; then
+#         relative_path=${file#../knowledge/note/data/notes/pages/}
+#         mkdir -p "./source/_posts/$(dirname "$relative_path")"
+#         cp "$file" "./source/_posts/$relative_path"
+#     fi
+# done
+# \cp -a ../knowledge/note/data/notes/wiki ./source
+
+rsync -ah --delete --exclude-from=<(find ../knowledge/note/data/notes/pages/ -type f -name "*.md" -exec grep -l "published: false" {} \; | sed "s|^../knowledge/note/data/notes/pages/||") ../knowledge/note/data/notes/pages/ ./source/_posts/ 
+rsync -ah --delete ../knowledge/note/data/notes/wiki ./source 
+
 # \cp -a ../knowledge/note/data/notes/game ./source
 # \cp -a ../knowledge/note/data/notes/about ./source
 # \cp -a ../knowledge/note/data/notes/pic ./source
